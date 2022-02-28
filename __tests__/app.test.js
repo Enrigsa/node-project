550d79d078bafc4c5cda903580bd4bde7a394f8b
enrique <global>
2022-02-28 13:03:49 +0100

    new test with jest

--- __tests__/app.test.js
+++
@@ -1,5 +0,0 @@
-const {port} = require ('../app.js');
-
-test("App.js test", async () =>{
-    expect(port).toBe("4000")
-})
---
+++ __tests__/trial.test.js
@@ -0,0 +1,5 @@
+const {year} = require ('../trial.js');
+
+test("App.js test", async () =>{
+    expect(year).toBe(2022)
+})
